f29fa21e6aa5f4f4d86aee9ce214cd71fba660eb
albin <yourname@example.com>
2025-02-19 07:49:47 +0530

    basic math operation completed

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function add(num1,num2){
+    return num1+num2;
+}
+
+function subtraction (num1,num2){
+    return num1-num2;
+}
+
+function division(num1,num2){
+    return num1-num2;
+}
+
+
+function multiplication(num1,num2){
+    return num1*num2;
+}
