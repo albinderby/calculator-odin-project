74682ebae74b2f578e2f46fa602cc648bc3e14d0
albin <yourname@example.com>
2025-02-19 13:13:25 +0530

    completed calculator project Note:without looking secondSolution

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-let num1;
-let num2;
-let operator;
+let num1="";
+let num2="";
+let operator="";
 
 
 function addition(num1,num2){
@@ -22,20 +22,72 @@ function multiplication(num1,num2){
 
 
 
-function operator(operator,num1,num2){
+function operatoration(operator,num1,num2){
 
     switch (operator){
         case "/":
-            division(num1,num2);
-            break;
+         return   division(num1,num2);
         case "*":
-            multiplication(num1,num2);
-            break;
+         return   multiplication(num1,num2);
         case "+":
-            addition(num1,num2);
-            break;
+        return    addition(num1,num2);
         case "-":
-            subtraction(num1,num2);
-            break;
+        return    subtraction(num1,num2);
     }
 }
+
+
+let operatorFlag=undefined;
+function getNumberEventHandler(e){
+   
+    if(operatorFlag===undefined){
+        num1+=e.target.textContent;
+    }else{
+        num2+=e.target.textContent;
+    }
+   display();
+}
+function getOperatorEventHandler(e){
+    if(operator===""){
+            operator=e.target.textContent;
+        }
+else{
+    answer();
+    operator=e.target.textContent;
+}
+    operatorFlag=true;
+    display();
+}
+function getUserInput(){
+    const numbers=document.querySelectorAll(".number");
+    for(let i=0;i<numbers.length;i++){
+        numbers[i].addEventListener("click",getNumberEventHandler)
+    }
+
+    const operators=document.querySelectorAll(".operator");
+    for(let i=0;i<operators.length;i++){
+        operators[i].addEventListener("click",getOperatorEventHandler)
+    }
+}
+
+function equalToButton(){
+    const equalToButton=document.querySelector(".answer");
+    equalToButton.addEventListener("click",answer);
+}
+ 
+function answer(){  
+    num1= operatoration(operator,Number(num1),Number(num2));
+    num2="";
+    operator="";
+    operatorFlag=false;
+    display();
+ 
+}
+
+
+function display(){
+    const display=document.querySelector(".display");
+    display.textContent=num1+operator+num2;
+}
+getUserInput();
+equalToButton();
